ecc2501c62122d862404b8ba9b07cd9eb207cf28
xiaoliang <xiaoliang_@outlook.com>
2023-01-30 10:59:15 +0800

    todo

--- middleware/trace/trace.go
+++ middleware/trace/trace.go
@@ -3,5 +3,8 @@ package trace
 import "wsproxy/middleware"
 
 func Middleware() middleware.Middleware {
-	panic("implement me")
+	// TODO; 链路追踪
+	return func(next middleware.Handler) middleware.Handler {
+		return next
+	}
 }
